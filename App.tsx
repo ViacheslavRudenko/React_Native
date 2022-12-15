930c6d5cfc2a46be4439fbd92ef223d5b355c95e
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-12-15 13:41:57 +0200

    new component  - ToDoBlock

--- App.tsx
+++ App.tsx
@@ -1,37 +1,13 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import Navbar from "./src/Components/Navbar/Navbar";
-import AddToDo from "./src/Components/ToDo/Input/ToDa";
-import { ToDoTitleType, ToDoType } from "./src/Components/ToDo/ToDo";
-import ToDoList from "./src/Components/ToDo/List/ToDoList";
+import ToDoBlock from "./src/Components/ToDo/ToDoBlock";
 
 export default function App() {
-  const [toDoArr, setToDoArr] = useState<ToDoType[] | []>([]);
-
-  const addToDo = (title: ToDoTitleType): void => {
-    const newData: ToDoType = {
-      id: Date.now().toString(),
-      title,
-    };
-
-    setToDoArr([...toDoArr, newData]);
-  };
-
   return (
     <View>
       <Navbar />
-      <View style={styles.container}>
-        <AddToDo onSubmit={addToDo} />
-        <ToDoList dataArr={toDoArr} />
-      </View>
+      <ToDoBlock />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 30,
-    paddingVertical: 20,
-  },
-});
---
+++ src/Components/ToDo/ToDoBlock.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import AddToDo from "./Input/ToDa";
+import ToDoList from "./List/ToDoList";
+import { ToDoTitleType, ToDoType } from "./ToDo";
+
+const ToDoBlock = () => {
+  const [toDoArr, setToDoArr] = useState<ToDoType[] | []>([]);
+
+  const addToDo = (title: ToDoTitleType): void => {
+    const newData: ToDoType = {
+      id: Date.now().toString(),
+      title,
+    };
+
+    setToDoArr([...toDoArr, newData]);
+  };
+  return (
+    <View style={styles.container}>
+      <AddToDo onSubmit={addToDo} />
+      <ToDoList dataArr={toDoArr} />
+    </View>
+  );
+};
+
+export default ToDoBlock;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+});
