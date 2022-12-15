c105f74a3a422a3530fbf80134cb59f4485efa90
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-12-15 13:23:26 +0200

    add navbar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navbar from "./src/Components/Navbar/Navbar";
+import AddToDo from "./src/Components/ToDo/ToDa";
+import { ToDoTitleType, ToDoType } from "./src/Types/ToDo";
 
 export default function App() {
+  const [toDoArr, setToDoArr] = useState([{}]);
+
+  const addToDo = (title: ToDoTitleType): void => {
+    const newData: ToDoType = {
+      id: Date.now().toString(),
+      title,
+    };
+
+    setToDoArr([...toDoArr, newData]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Emma,How are you?</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar />
+      <View style={styles.container}>
+        <AddToDo onSubmit={addToDo} />
+        <View></View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
