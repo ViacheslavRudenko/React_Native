7dc39a32873b9765f7d70cac4e61a4abe432b5b1
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-12-26 15:37:16 +0200

    change baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.REACT_APP_API || "https://63a025327aaf11ceb8a897ef.mockapi.io",
+  baseURL: "https://63a025327aaf11ceb8a897ef.mockapi.io",
 });
-console.log(process.env.DA);
 
 export default instance;
